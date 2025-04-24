43431f5acde8c97fdd9213c37542905453ee7054
DanielMuMe <daniel.murillom@uniagustiniana.edu.co>
2025-04-24 03:37:01 +0000

    Areglao

--- JS/api.js
+++ JS/api.js
@@ -1,4 +1,10 @@
-function mostrarApi(){
-    const app = document.getElementById("app");
-    app.innerHTML = "Api";
+async function conexionList(){
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1025');
+    const data = await res.json();
+    return data.results;
+}
+
+async function general(){
+    const infoPokes = await conexionList();
+    mostrarLista(infoPokes);
 }
--- JS/listas.js
+++ JS/listas.js
@@ -1,4 +1,19 @@
-function mostrarListas(){
+function mostrarLista(pokemones){
     const app = document.getElementById("app");
-    app.innerHTML = "Listas";
+    app.innerHTML = "Lista";
+    console.log(pokemones);
+
+    let pokes = "";
+
+    for (let i = 0; i < pokemones.length; i++) {
+        let id = pokemones[i].url.split("/")[6];
+        pokes += `
+        <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${pokemones[i].name}')">
+            <p>#${id}</p>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${pokemones[i].name}">
+            <p>${pokemones[i].name}</p>
+        </div>`;
+    }
+    app.innerHTML = pokes;
 }
+
